1f3b8b083db1fe90fc094543bc55dc72a7cbd908
PritiSingh93 <48630121+PritiSingh93@users.noreply.github.com>
2019-03-22 11:02:37 +0530

    Good

--- DailyActive/src/Day6/BroknLik.java
+++ DailyActive/src/Day6/BroknLik.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+// checked
 public class BroknLik {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "A:\\selenium-prerequesite-jar\\chromedriver.exe");
